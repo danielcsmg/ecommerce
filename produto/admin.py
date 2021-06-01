3c4dcec1ce3436a089502872cd06f4496ca93785
Daniel <daniel@pop-os.localdomain>
2021-06-01 08:41:46 -0300

    Nova atualizaçao 2

--- produto/admin.py
+++ produto/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from . import models
 
+class VariacaoInLine(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInLine
+    ]
+    
+
 admin.site.register(models.Produto)
+admin.site.register(models.Variacao)
--- produto/models.py
+++ produto/models.py
@@ -1,6 +1,8 @@
+from os import name
 from django.db import models
 from pathlib import Path
 from django.conf import settings
+from PIL import Image
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
@@ -24,18 +26,51 @@ class Produto(models.Model):
     @staticmethod
     def resize_image(img, new_width=800):
         img_full_path = Path(settings.MEDIA_ROOT, img.name)
-        img_pill = Image.open(img_full_path)
-        original_width, original_heigth = img_pill.size
-        print(original_heigth, original_width)
+        img_pil = Image.open(img_full_path)
+        original_width, original_heigth = img_pil.size
+
+        if original_width <= new_width:
+            print('Retornando. Largura original menor que a nova largura.')
+            img_pil.close()
+            return
+
+        new_heigth = round(new_width * original_heigth / original_width)
+
+        new_img = img_pil.resize((new_width, new_heigth), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+
+
+        print('Nova imagem')
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         max_image_size = 800
 
-        if imagem:
+    
+        if self.imagem:
             self.resize_image(self.imagem, max_image_size)
 
 
     def __str__(self):
         return self.nome
+
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
+    
