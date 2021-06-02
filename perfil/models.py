6403ff2f6d3a93de0e3c08f3dcfbc8c7f969a3eb
Daniel <daniel@pop-os.localdomain>
2021-06-02 10:10:41 -0300

    Models quase pronto

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
 
-# Create your models here.
+import re
+
+from utils import validacpf
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default='MG',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins')
+        )
+    )
+
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+
+    def clean(self):
+        error_message = {}
+
+        if not validacpf(self.cpf):
+            error_message['cpf'] = 'Digite um CPF válido.'
+
+
+    class Meta:
+        verbose_name_plural = 'Perfis'
